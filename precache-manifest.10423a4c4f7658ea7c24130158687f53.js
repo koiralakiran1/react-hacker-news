c9041b192f78e522214fced7700495ac1477b7b9
Kiran Koirala <koirala.kiran1@gmail.com>
2019-01-29 14:34:38 +0545

    Updates

---
+++ precache-manifest.10423a4c4f7658ea7c24130158687f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e33b511402e3330288",
+    "url": "/react-hacker-news/static/css/main.63193b13.chunk.css"
+  },
+  {
+    "revision": "72e33b511402e3330288",
+    "url": "/react-hacker-news/static/js/main.72e33b51.chunk.js"
+  },
+  {
+    "revision": "94c4f4d0e5572e1ad98b",
+    "url": "/react-hacker-news/static/js/1.94c4f4d0.chunk.js"
+  },
+  {
+    "revision": "9d1cecd383bde5c3a2cd",
+    "url": "/react-hacker-news/static/js/runtime~main.9d1cecd3.js"
+  },
+  {
+    "revision": "52b15ac8a8301290a03c1648e41a2877",
+    "url": "/react-hacker-news/index.html"
+  }
+];
